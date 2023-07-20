ffca40c246bf7b079465d9115c6fc1d66e2babfa
MeenaManogari <akshayahps@gmail.com>
2023-07-20 17:52:13 +0530

    feat!:Added features for backend and connected project with REST API and MongoDB

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
+
+// router.get("/", getGoals);
+
+// router.post("/", setGoal);
+
+// router.put("/:id", updateGoal);
+
+// router.delete("/:id", deleteGoal);
+
+//as they have same paths.. instead put
+
+router.route("/").get(getGoals).post(setGoal);
+router.route("/:id").delete(deleteGoal).put(updateGoal);
+
+module.exports = router;
